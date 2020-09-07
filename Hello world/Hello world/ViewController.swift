159bd171dabd17d5d5a55cab4c5e018d07823ed9
Pavel Chernyshev <titan100695@yandex.ru>
2020-09-07 22:04:39 +0300

    create empty project

---
+++ Hello world/Hello world/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hello world
+//
+//  Created by Павел Чернышев on 07.09.2020.
+//  Copyright © 2020 Павел Чернышев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
