7e01ea9aca543fd86ad41e3e1b6147375784d6dd
Pavel Chernyshev <titan100695@yandex.ru>
2020-09-10 06:30:33 +0300

    remove empty lines

--- WeeklyFinder/WeeklyFinder/ViewController.swift
+++ WeeklyFinder/WeeklyFinder/ViewController.swift
@@ -22,11 +22,9 @@ class ViewController: UIViewController {
         dateComponents.day = Int(day)
         dateComponents.month  = Int(month)
         dateComponents.year  = Int(year)
-        
         let dateFormatter = DateFormatter()
-        dateFormatter.locale = Locale(identifier: "ru_Ru")
+        dateFormatter.locale = Locale(identifier: "ru_RU")
         dateFormatter.dateFormat = "EEEE"
-        
         guard let date = calendar.date(from: dateComponents) else { return }
         let weekday = dateFormatter.string(from: date)
         resultLable.text = weekday.capitalized
