777aeb049651d7fdad65c9e44599a060d3bcd60c
Pavel Chernyshev <titan100695@yandex.ru>
2020-09-11 23:12:37 +0300

    t-converter app. ui items setted and conections wrote

---
+++ TConverter/TConverter/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  TConverter
+//
+//  Created by Павел Чернышев on 11.09.2020.
+//  Copyright © 2020 Павел Чернышев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+
+    @IBOutlet weak var cLabel: UILabel!
+    @IBOutlet weak var fLabel: UILabel!
+    @IBOutlet weak var slider: UISlider!
+    
+    @IBAction func sliderChanged(_ sender: UISlider) {
+    }
+}
+
